d833d248c9e132c1e8ee706ffcd38afe0bc23abd
Nagendra Mishr <nmishr@microsoft.com>
2024-06-22 13:20:57 -0400

    added PollTimeout

--- BackendOptions.cs
+++ BackendOptions.cs
@@ -3,6 +3,7 @@ public class BackendOptions : IBackendOptions
     public int Port { get; set; }
     public int PollInterval { get; set; }
     public int SuccessRate { get; set; }
+    public int PollTimeout { get; set; }
     public int Timeout { get; set; }
 
     public List<BackendHost>? Hosts { get; set; }
