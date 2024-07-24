5ba074633d10decbb80fee3a9eebc42dfc1e11a6
Nagendra Mishr <nmishr@microsoft.com>
2024-07-24 13:39:57 -0400

    Added Workers, added cancelation token during startup

--- Interfaces.cs
+++ Interfaces.cs
@@ -2,7 +2,7 @@ public interface IBackendService
 {
     void Start(CancellationToken cancellationToken);
     public List<BackendHost> GetActiveHosts();
-    public Task waitForStartup(int timeout);
+    public Task waitForStartup(int timeout, CancellationToken cancellationToken);
 
 }
 
@@ -19,6 +19,7 @@ public interface IBackendOptions {
     int PollTimeout { get; set; }
     int SuccessRate { get; set; }
     int Timeout { get; set; }
+    int Workers { get; set; }
 
     List<BackendHost>? Hosts { get; set; }
     HttpClient? Client { get; set; }
